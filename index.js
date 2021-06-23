0ef7feb547f7edf55388af41e6b0625c5330968e
talnoznisky <tal.noznisky@gmail.com>
2021-06-22 23:50:08 -0500

    first draft

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// const ends = ['yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyye',
+//               'yyyyyyyyyyyyyy-yyyyyyyyyyyyyyyyyy-yyyyyyyyyyyyyyyye',
+//               'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyye'] 
+// const goodb = 'goodby'              
+
+// for(let i = 0; i < ends.length; i++){
+//     const newLetter = document.createElement('div')
+//     newLetter.classList.add('letter')
+
+//     let goodbye = document.querySelector('.goodbye')
+//     goodbye.append(newLetter.innerText = lettersToAdd.charAt(i))
+// }
+ 
